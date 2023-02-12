8970bca78238742e7b2ede5b62f97fa86779ad92
Prahlad Dutta <pradutta5@publicisgroupe.net>
2023-02-12 23:06:48 +0530

    Initial commit

---
+++ src/main/java/techiearchive/lab/App.java
@@ -0,0 +1,13 @@
+package techiearchive.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
