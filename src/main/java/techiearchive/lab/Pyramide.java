8c584cfef4bffe168864a5e5006872cd21af6e7c
Techie Archive <72217019+techiearchive@users.noreply.github.com>
2023-02-14 13:48:38 +0530

    Create Pyramide.java

---
+++ src/main/java/techiearchive/lab/Pyramide.java
@@ -0,0 +1,19 @@
+package techiearchive.lab;
+
+public class Pyramide {
+    public static void main(String[] args) {
+        System.out.println();
+        for(int i=1; i<=5; i++) {
+            for(int j=5; j>=i; j--){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print("*");
+            }
+            for(int l=1; l<=i; l++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
