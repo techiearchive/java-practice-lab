d4b8a0f86139c3a98a1c8715ed409aa5cda808aa
Techie Archive <72217019+techiearchive@users.noreply.github.com>
2023-02-14 13:50:48 +0530

    Create StringTest.java

---
+++ src/main/java/techiearchive/lab/StringTest.java
@@ -0,0 +1,8 @@
+package techiearchive.lab;
+
+public class StringTest {
+    public static void main(String[] args) {
+         String str = 10+20+"hello"+30+40+"java"+50+60;
+        System.out.print(str);
+    }
+}
